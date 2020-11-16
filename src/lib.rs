4dae497f05a0f6dd22e536e29bfedb417a5ca711
Odin Dutton <odindutton@gmail.com>
2020-11-16 23:03:37 +1100

    Extract sequences mod

--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,11 @@ use serde_json::value::Value as JsonValue;
 use sled::{Config, Db};
 use uuid::Uuid;
 
-use std::collections::HashSet;
-use std::sync::atomic::{AtomicU64, Ordering};
-use std::sync::{Arc, Mutex, RwLock};
-
 const DEFAULT_LIMIT: usize = 1000;
+
+mod sequences;
+
+use crate::sequences::Sequences;
 
 #[derive(Deserialize, Serialize, Debug, PartialEq, Clone)]
 pub struct NewEvent {
@@ -166,99 +166,5 @@ impl EventStore {
         }
 
         Ok(events)
-    }
-}
-
-#[derive(Clone)]
-struct Sequences {
-    in_flight_sequences: Arc<RwLock<HashSet<u64>>>,
-    finished_sequences: Arc<RwLock<HashSet<u64>>>,
-    readers_count: Arc<AtomicU64>,
-    generate_sequence_lock: Arc<Mutex<()>>,
-}
-
-impl Sequences {
-    fn new() -> Self {
-        Self {
-            // List of in-flight sequences
-            in_flight_sequences: Arc::new(RwLock::new(HashSet::new())),
-            // List of sequences that have been marked as "finished" ready for removal
-            finished_sequences: Arc::new(RwLock::new(HashSet::new())),
-            // Number of threads currently reading. This is used to ensure we don't remove
-            // in-flight sequences while threads might be using them.
-            readers_count: Arc::new(AtomicU64::new(0)),
-            // Used to ensure newly generated sequences is marked as in-flight before they're used.
-            generate_sequence_lock: Arc::new(Mutex::new(())),
-        }
-    }
-
-    fn min_in_flight_sequence(&self) -> Option<u64> {
-        self.in_flight_sequences
-            .read()
-            .unwrap()
-            .iter()
-            .min()
-            .copied()
-    }
-
-    fn start_reading(&self) {
-        // Lock removing of in-flight sequences while we fetch events
-        self.readers_count.fetch_add(1, Ordering::SeqCst);
-    }
-
-    fn mark_sequence_in_flight(&self, sequence: u64) {
-        self.in_flight_sequences.write().unwrap().insert(sequence);
-    }
-
-    fn mark_sequence_finished(&self, sequence: u64) {
-        self.finished_sequences.write().unwrap().insert(sequence);
-    }
-
-    fn finished_reading(&self) {
-        self.readers_count.fetch_sub(1, Ordering::SeqCst);
-
-        // If there are no other readers we're safe to remove in-flight sequences
-        if self.readers_count.load(Ordering::SeqCst) == 0 {
-            // Read the finished sequences first
-            let mut finished_sequences = self.finished_sequences.write().unwrap();
-
-            // Remove any sequences that are no longer in flight
-            let mut in_flight_sequences = self.in_flight_sequences.write().unwrap();
-            for sequence in finished_sequences.drain() {
-                in_flight_sequences.remove(&sequence);
-            }
-        }
-    }
-
-    /// When generating a sequence we need to do it in a lock so that we can be sure that no other
-    /// IDs get generated before we mark the new sequence as in-flight.
-    fn generate(&self, db: &Db) -> sled::Result<NewSequence> {
-        match self.generate_sequence_lock.lock() {
-            Ok(_) => {
-                let sequence = db.generate_id()?;
-                // Start sequence at 1.
-                let sequence = sequence + 1;
-                self.mark_sequence_in_flight(sequence);
-                Ok(NewSequence::new(sequence, &self))
-            }
-            Err(e) => panic!("{}", e),
-        }
-    }
-}
-
-struct NewSequence<'a> {
-    value: u64,
-    sequences: &'a Sequences,
-}
-
-impl<'a> NewSequence<'a> {
-    fn new(value: u64, sequences: &'a Sequences) -> Self {
-        Self { value, sequences }
-    }
-}
-
-impl Drop for NewSequence<'_> {
-    fn drop(&mut self) {
-        self.sequences.mark_sequence_finished(self.value);
     }
 }
---
+++ src/sequences.rs
@@ -0,0 +1,99 @@
+use sled::Db;
+
+use std::collections::HashSet;
+use std::sync::atomic::{AtomicU64, Ordering};
+use std::sync::{Arc, Mutex, RwLock};
+
+#[derive(Clone)]
+pub struct Sequences {
+    in_flight_sequences: Arc<RwLock<HashSet<u64>>>,
+    finished_sequences: Arc<RwLock<HashSet<u64>>>,
+    readers_count: Arc<AtomicU64>,
+    generate_sequence_lock: Arc<Mutex<()>>,
+}
+
+impl Sequences {
+    pub fn new() -> Self {
+        Self {
+            // List of in-flight sequences
+            in_flight_sequences: Arc::new(RwLock::new(HashSet::new())),
+            // List of sequences that have been marked as "finished" ready for removal
+            finished_sequences: Arc::new(RwLock::new(HashSet::new())),
+            // Number of threads currently reading. This is used to ensure we don't remove
+            // in-flight sequences while threads might be using them.
+            readers_count: Arc::new(AtomicU64::new(0)),
+            // Used to ensure newly generated sequences is marked as in-flight before they're used.
+            generate_sequence_lock: Arc::new(Mutex::new(())),
+        }
+    }
+
+    /// When generating a sequence we need to do it in a lock so that we can be sure that no other
+    /// IDs get generated before we mark the new sequence as in-flight.
+    pub fn generate(&self, db: &Db) -> sled::Result<NewSequence> {
+        match self.generate_sequence_lock.lock() {
+            Ok(_) => {
+                let sequence = db.generate_id()?;
+                // Start sequence at 1.
+                let sequence = sequence + 1;
+                self.mark_sequence_in_flight(sequence);
+                Ok(NewSequence::new(sequence, &self))
+            }
+            Err(e) => panic!("{}", e),
+        }
+    }
+
+    pub fn min_in_flight_sequence(&self) -> Option<u64> {
+        self.in_flight_sequences
+            .read()
+            .unwrap()
+            .iter()
+            .min()
+            .copied()
+    }
+
+    pub fn start_reading(&self) {
+        // Lock removing of in-flight sequences while we fetch events
+        self.readers_count.fetch_add(1, Ordering::SeqCst);
+    }
+
+    fn mark_sequence_in_flight(&self, sequence: u64) {
+        self.in_flight_sequences.write().unwrap().insert(sequence);
+    }
+
+    fn mark_sequence_finished(&self, sequence: u64) {
+        self.finished_sequences.write().unwrap().insert(sequence);
+    }
+
+    pub fn finished_reading(&self) {
+        self.readers_count.fetch_sub(1, Ordering::SeqCst);
+
+        // If there are no other readers we're safe to remove in-flight sequences
+        if self.readers_count.load(Ordering::SeqCst) == 0 {
+            // Read the finished sequences first
+            let mut finished_sequences = self.finished_sequences.write().unwrap();
+
+            // Remove any sequences that are no longer in flight
+            let mut in_flight_sequences = self.in_flight_sequences.write().unwrap();
+            for sequence in finished_sequences.drain() {
+                in_flight_sequences.remove(&sequence);
+            }
+        }
+    }
+}
+
+pub struct NewSequence<'a> {
+    pub value: u64,
+    sequences: &'a Sequences,
+}
+
+impl<'a> NewSequence<'a> {
+    pub fn new(value: u64, sequences: &'a Sequences) -> Self {
+        Self { value, sequences }
+    }
+}
+
+impl Drop for NewSequence<'_> {
+    fn drop(&mut self) {
+        self.sequences.mark_sequence_finished(self.value);
+    }
+}
